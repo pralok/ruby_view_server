ccbf9e9bd43acb3730da861438ff509ff2ee2b0d
Pralok chhajed <Pralok.chhajed@gmail.com>
2015-01-23 19:11:07 -0500

    added todays date and an array method in erb_examples.rb file

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,16 @@
 require 'erb'
+require 'Date'
 
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+
+Date_today = Date.today
+Date_template = ERB.new "Today's date is : <%= Date_today %>"
+puts Date_template.result(binding)
+
+
+arr = ['pralok','pittu','pranesh']
+Names_template = ERB.new"<% arr.each do |name| %> Name is  <%= name %> <% end %>"
+puts Names_template.result(binding)
